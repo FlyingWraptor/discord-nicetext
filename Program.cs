c534f25f0a15f0ee0c1cbdc80db228569639169d
Wraptor <wraptor@wheezie.be>
2018-08-21 21:20:59 +0200

    Added program

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace discord_nicetext
+{
+    class Program
+    {
+        static string[] numberStrings = new string[] { ":zero:", ":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:" };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input text: ");
+            char[] arrInput = Console.ReadLine().ToCharArray();
+            
+            string sOutput = "";
+            for(ushort i=0;i<arrInput.Length;i++)
+            {
+                if(Char.IsLetter(arrInput[i]))
+                {
+                    sOutput += $":regional_indicator_{Char.ToLower(arrInput[i])}: ";
+                }   
+                else if(Char.IsDigit(arrInput[i]))
+                {
+                    sOutput += numberStrings.GetValue(Convert.ToInt32(arrInput[i].ToString())) + " ";
+                }
+                else if (Char.IsWhiteSpace(arrInput[i]))
+                {
+                    sOutput += "  ";
+                }
+                else
+                {
+                    sOutput += arrInput[i];
+                }
+            }
+
+            Console.WriteLine(sOutput);
+        }
+    }
+}
